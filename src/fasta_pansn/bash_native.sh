6b366ba39bf1683a57b8eda729dd3bc15dc7ed41
VKhang-Bui <btai37999@gmail.com>
2026-07-30 13:45:05 +0700

    fix logic create pansn

--- src/fasta_pansn/bash_native.sh
+++ src/fasta_pansn/bash_native.sh
@@ -3,13 +3,29 @@
 # Script chuyển đổi header FASTA sang chuẩn PanSN bằng Bash thuần (dùng sed)
 # Cách dùng: ./bash_native.sh <thu_muc_input> <thu_muc_output>
 
-input=$1
-output=$2
+input="/home/vkhang-bui/1.HocViec/projects/pangenom/data/raw"
+output="/home/vkhang-bui/1.HocViec/projects/pangenom/data"
 
 mkdir -p "$output"
-
+> "$output/pansn.fasta"  # Tạo file output rỗng
 for f in "$input"/*.fa; do
-    [ -e "$f" ] || continue
+
     name=$(basename "$f" .fa)
-    sed "s/>/>${name}#1#/" "$f" > "${output}/${name}_pansn.fa"
+
+
+    # Biến đếm thứ tự read/contig trong file (bắt đầu từ 1)
+    read_idx=1
+        
+    # Vòng lặp 2: Đọc từng dòng trong tập tin FASTA
+    while IFS= read -r line || [ -n "$line" ]; do
+        # Kiểm tra nếu dòng hiện tại là Header (bắt đầu bằng ký tự '>')
+        if [[ "$line" =~ ^\> ]]; then
+            # Dùng sed thay thế '>' bằng '>Sample#Haplotype#read_N_' kết hợp tên gốc
+            echo "$line" | sed "s/^>/>${name}#1#read_${read_idx}_/" >> "$output/pansn.fasta"   
+            read_idx=$((read_idx + 1))
+        else
+            echo "$line" >> "$output/pansn.fasta"
+        fi 
+    done < "$f"
+    echo "✓ Đã xử lý file '$name.fa' với $((read_idx - 1)) reads/contigs."
 done
